5f90054cdc1b01be6b5da90f6859b440e22f4e20
Aman Upadhyay <58130120+AmanUpadhyay8@users.noreply.github.com>
2020-10-24 14:46:12 +0530

    Add files via upload

---
+++ SimpleHelloWorldProgram.java
@@ -0,0 +1,12 @@
+
+public class IamAMAN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Aman");
+		System.out.println("Voila The Program Works!");
+		System.out.println("I am Learning .");
+		
+	}
+
+}
